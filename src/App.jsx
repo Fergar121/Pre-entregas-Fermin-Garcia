161b3b3adfada590ac5006cf772973619d8d8b2c
Fermin Garcia <fermingarcia389@gmail.com>
2022-11-23 21:10:38 -0300

    subida a github

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from '/componentes/Navbar'
+import ItemListContainer from '/componentes/ItemListContainer'
+
+const App = () => {
+  return (
+    <>
+
+    <Navbar />
+    <ItemListContainer mensaje="buenas tardes" />
+
+    </>
+  )
+}
+
+export default App; 
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+            <header>
+                <nav>
+                    <h1>hola</h1>
+                    <p>esto es una prueba</p>
+                </nav>
+            </header>
+        )
+}
+
+export default Navbar;
